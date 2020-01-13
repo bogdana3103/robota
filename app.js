316e7367cbb79fdc3f851f44d0f5135910af5002
Bogdana Zaparnuk <zaparnukbogdana@gmail.com>
2020-01-13 21:26:32 +0200

    function

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-// for (var i = 0; i < 100; i++) {
-//     if (i % 3)
-// }
+
+
+function fizzBuzz ( firstNum, secondNum ) {
+    for (var i = firstNum; i < secondNum; i++) {
+
+        if (i%3 === 0 && i%5 === 0) {
+            document.write('thirty-five <br/>');
+        } else if (i%5 === 0) {
+            document.write('five')
+        } else if (i%3 === 0 ) {
+            document.write('tree') 
+        } else {
+            document.write(i + '<br/>');
+        }
+        
+    
+    }
+}
+
+fizzBuzz ( 20, 30)
+
